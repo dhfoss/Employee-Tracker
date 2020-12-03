cae9e2671c148d00cea48cdc5ebc1389695372cd
Daniel Hawthorne-Foss <dhfoss89@gmail.com>
2020-12-02 21:12:20 -0500

    Added queries for viewing Employees

--- index.js
+++ index.js
@@ -35,16 +35,19 @@ function init() {
             type: 'list',
             name: 'init',
             message: 'What would you like to do?',
-            choices: ['Add Departments', 'Add Roles', 'Add Employees', 'View Departments', 'View Roles', 'View Employess', 'Exit Employee Tracker']
+            choices: ['Add Departments', 'Add Roles', 'Add Employees', 'View Departments', 'View Roles', 'View Employees', 'Exit Employee Tracker']
         }
     ]).then((answers) => {
         switch(answers.init) {
             case 'Exit Employee Tracker':
                 connection.end();
-                console.log('Goodbye')
+                console.log('Goodbye');
                 break;
             case 'View Departments':
                 viewDepartments();
+                break;
+            case 'View Employees':
+                viewEmployees();
                 break;
             case 'View Roles':
                 viewRoles();
@@ -99,6 +102,74 @@ function viewRoles() {
 }
 
 // View Employees
+function viewEmployees() {
+    inquirer.prompt([
+        {
+            name: 'sortBy',
+            type: 'list',
+            message: 'How would you like to sort your employees?',
+            choices: ['Last name', 'Manager', 'Department']
+        }
+    ]).then((answers) => {
+        switch(answers.sortBy) {
+            case 'Last name':
+                sortByLastName();
+                break;
+            case 'Manager':
+                sortByManager();
+                break;
+            case 'Department':
+                sortByDepartment();
+                break;
+        }
+    })
+}
+
+function sortByLastName() {
+connection.query(`SELECT e.last_name AS 'Last Name', e.first_name AS 'First Name', r.title AS 'Role', d.name AS 'Department'
+                  FROM employees e
+                  JOIN roles r
+                  ON e.role_id = r.id
+                  LEFT JOIN departments d
+                  ON r.department_id = d.id
+                  ORDER BY e.last_name`, (err, res) => {
+                    if (err) throw err;
+                    console.log('\n\n')
+                    console.table(res);
+                    init();
+                  });
+
+}
+function sortByManager() {
+    connection.query(`SELECT e.last_name AS 'Employee Last Name', e.first_name AS 'Employee First Name', m.last_name AS 'Manager'
+                      FROM employees e
+                      LEFT JOIN employees m
+                      ON e.manager_id = m.id
+                      ORDER BY m.last_name, e.last_name`, (err, res) => {
+                        if (err) throw err;
+                        console.log('\n\n')
+                        console.table(res);
+                        init();
+    });
+
+}
+
+function sortByDepartment() {
+    connection.query(`SELECT d.name AS 'Department', e.last_name AS 'Last Name', e.first_name AS 'First Name', r.title AS 'Role'
+                      FROM employees e
+                      JOIN roles r
+                      ON e.role_id = r.id
+                      LEFT JOIN departments d
+                      ON r.department_id = d.id
+                      ORDER BY d.name, e.last_name`, (err, res) => {
+                        if (err) throw err;
+                        console.log('\n\n')
+                        console.table(res);
+                        init();
+    });
+}
+
+
 
 
 
