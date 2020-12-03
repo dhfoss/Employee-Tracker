dd6c56a4302260d97d151c3374a51c1bbcce4b1b
Daniel Hawthorne-Foss <dhfoss89@gmail.com>
2020-12-02 20:29:12 -0500

    Added two SQL queries

--- index.js
+++ index.js
@@ -9,42 +9,97 @@ const connection = mysql.createConnection({
     database: 'company'
 });
 
-connection.connect();
+// connection.connect();
 
-connection.query('SELECT * FROM employees', (err, res) => {
-    if (err) throw err;
-    console.table(res);
-});
+// connection.query('SELECT * FROM employees', (err, res) => {
+//     if (err) throw err;
+//     console.table(res);
+// });
 
-connection.end();
+// connection.end();
 
 
 
 
 // What would you like to do?
 
+console.log('\n\nWelcome to the Employee Tracker\n\n===============================');
+connection.connect();
+init();
 
+
+function init() {
+    console.log('\n\n')
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'init',
+            message: 'What would you like to do?',
+            choices: ['Add Departments', 'Add Roles', 'Add Employees', 'View Departments', 'View Roles', 'View Employess', 'Exit Employee Tracker']
+        }
+    ]).then((answers) => {
+        switch(answers.init) {
+            case 'Exit Employee Tracker':
+                connection.end();
+                console.log('Goodbye')
+                break;
+            case 'View Departments':
+                viewDepartments();
+                break;
+            case 'View Roles':
+                viewRoles();
+                break;
+        }
+    })
+}
+
+
+
+
+
+// =============
+// ADD FUNCTIONS
+// =============
 // Add departments
-
 // Add Roles
-
 // Add Employees
 
 
 
+
+
+// ==============
+// VIEW FUNCTIONS
+// ==============
+
+
 // View departments
+function viewDepartments() {
+    connection.query(`SELECT name AS 'Departments' FROM departments`, (err, res) => {
+        if (err) throw err;
+        console.log('\n\n')
+        console.table(res);
+        init();
+    });
+}
 
 // View Roles
+function viewRoles() {
+    connection.query(`SELECT r.title AS 'Role', d.name AS 'Department', r.salary AS 'Salary'
+                      FROM company.roles r
+                      JOIN departments d
+                      ON r.department_id = d.id
+                      ORDER BY r.department_id`, 
+                      (err, res) => {
+                          if (err) throw err;
+                          console.log('\n\n')
+                          console.table(res);
+                          init();
+                      });
+}
 
 // View Employees
 
 
 
 // Update Employee Roles
-
-
-
-// inquirer.prompt({
-
-
-// })
