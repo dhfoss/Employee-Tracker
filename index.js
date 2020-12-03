a5d2ad3328b8a192f499239a1e4e4eef900509c0
Daniel Hawthorne-Foss <dhfoss89@gmail.com>
2020-12-03 13:44:51 -0500

    Add Role Function working

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const cTable = require('console.table');
 const inquirer = require('inquirer');
 const mysql = require('mysql');
+const Department = require(__dirname + '/classes/Department.js');
+const Role = require(__dirname + '/classes/Role.js');
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -9,24 +11,12 @@ const connection = mysql.createConnection({
     database: 'company'
 });
 
-// connection.connect();
-
-// connection.query('SELECT * FROM employees', (err, res) => {
-//     if (err) throw err;
-//     console.table(res);
-// });
-
-// connection.end();
-
-
-
 
 // What would you like to do?
 
 console.log('\n\nWelcome to the Employee Tracker\n\n===============================');
 connection.connect();
 init();
-
 
 function init() {
     console.log('\n\n')
@@ -35,13 +25,19 @@ function init() {
             type: 'list',
             name: 'init',
             message: 'What would you like to do?',
-            choices: ['Add Departments', 'Add Roles', 'Add Employees', 'View Departments', 'View Roles', 'View Employees', 'Exit Employee Tracker']
+            choices: ['Add Department', 'Add Role', 'Add Employee', 'View Departments', 'View Roles', 'View Employees', 'Exit Employee Tracker']
         }
     ]).then((answers) => {
         switch(answers.init) {
             case 'Exit Employee Tracker':
                 connection.end();
                 console.log('Goodbye');
+                break;
+            case 'Add Department':
+                addDepartment();
+                break;
+            case 'Add Role':
+                addRole();
                 break;
             case 'View Departments':
                 viewDepartments();
@@ -58,13 +54,103 @@ function init() {
 
 
 
+// // Regular expression for VARCHAR(30)
+// /.{1,30}/
+
 
 
 // =============
 // ADD FUNCTIONS
 // =============
 // Add departments
+function addDepartment() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the new department?'
+        }
+    ]).then((answers) => {
+        insertDepartment(answers.name);
+    });
+}
+
+function insertDepartment(newDepot) {
+    connection.query('INSERT INTO departments SET ?', new Department(newDepot), (err, res) => {
+        if (err) throw err;
+        console.log(`Successfully added ${newDepot} to Departments`);
+        init();
+    });
+}
+
 // Add Roles
+// title, salary, department
+function addRole() {
+    const array = [];
+    getDepartmentsAsync()
+    .then(data => {
+            for (let i=0; i<data.length; i++) {
+                array.push(data[i])
+            }
+        })
+    .catch(err => {
+        console.log(err);
+    });
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of the new role?'
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'What is the salary of the new role?'
+        },
+        {
+            type: 'list',
+            name: 'department',
+            message: 'In which department is the new role?',
+            choices: array
+        }
+    ]).then(answers => {
+        let departmentId;
+        for (let i = 0; i < array.length; i++) {
+            if (answers.department === array[i].name) {
+                departmentId = array[i].id;
+            }
+        }
+        insertRole(answers.title, answers.salary, departmentId);
+    })
+}
+
+function insertRole(title, salary, department_id) {
+    connection.query('INSERT INTO roles SET ?', new Role(title, salary, department_id), (err, res) => {
+        if (err) throw err;
+        console.log(`Successfully added ${title} to Roles`);
+        init();
+    });
+
+}
+
+function getDepartmentsAsync() {
+    return new Promise((resolve, reject) => {
+        connection.query(`SELECT * FROM departments`, (err, data) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(data);
+        })
+    })
+}
+
+
+
+
+
+
+
 // Add Employees
 
 
@@ -140,6 +226,7 @@ connection.query(`SELECT e.last_name AS 'Last Name', e.first_name AS 'First Name
                   });
 
 }
+
 function sortByManager() {
     connection.query(`SELECT e.last_name AS 'Employee Last Name', e.first_name AS 'Employee First Name', m.last_name AS 'Manager'
                       FROM employees e
